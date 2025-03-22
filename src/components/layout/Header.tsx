3ce786b5790d7addf13d2e2112e0fa883b84e932
REXIN <184462342+rexinscfu@users.noreply.github.com>
2025-03-22 19:44:38 +0000

    Add blog to navbar and create simple hello example post for 2025

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -13,6 +13,7 @@ const navLinks = [
   { href: '/about', label: 'About' },
   { href: '/projects', label: 'Projects' },
   { href: '/services', label: 'Services' },
+  { href: '/blog', label: 'Blog' },
   { href: '/contact', label: 'Contact' },
 ];
 
